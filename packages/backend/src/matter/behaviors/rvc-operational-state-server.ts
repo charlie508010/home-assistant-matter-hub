7f17c92680a23be3f2c0cf6a1cc61a56b4057012
RiDDiX <github@egu-rel.de>
2026-01-27 00:22:47 +0100

    fix: move super.initialize() to beginning in rvc behavior servers

--- packages/backend/src/matter/behaviors/rvc-operational-state-server.ts
+++ packages/backend/src/matter/behaviors/rvc-operational-state-server.ts
@@ -19,10 +19,10 @@ class RvcOperationalStateServerBase extends Base {
   declare state: RvcOperationalStateServerBase.State;
 
   override async initialize() {
+    await super.initialize();
     const homeAssistant = await this.agent.load(HomeAssistantEntityBehavior);
     this.update(homeAssistant.entity);
     this.reactTo(homeAssistant.onChange, this.update);
-    await super.initialize();
   }
 
   private update(entity: HomeAssistantEntityInformation) {
