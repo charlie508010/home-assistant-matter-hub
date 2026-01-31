9acd7cfbb01d1de51475bf5bff413b84134908aa
RiDDiX <github@egu-rel.de>
2026-01-31 17:28:14 +0100

    fix: add sensor type factories to enable entity mapping override (#73)

--- packages/backend/src/matter/endpoints/legacy/create-legacy-endpoint-type.ts
+++ packages/backend/src/matter/endpoints/legacy/create-legacy-endpoint-type.ts
@@ -27,6 +27,12 @@ import { MediaPlayerDevice } from "./media-player/index.js";
 import { RemoteDevice } from "./remote/index.js";
 import { SceneDevice } from "./scene/index.js";
 import { ScriptDevice } from "./script/index.js";
+import { AirQualitySensorType } from "./sensor/devices/air-quality-sensor.js";
+import { FlowSensorType } from "./sensor/devices/flow-sensor.js";
+import { HumiditySensorType } from "./sensor/devices/humidity-sensor.js";
+import { IlluminanceSensorType } from "./sensor/devices/illuminance-sensor.js";
+import { PressureSensorType } from "./sensor/devices/pressure-sensor.js";
+import { TemperatureSensorType } from "./sensor/devices/temperature-sensor.js";
 import { SensorDevice } from "./sensor/index.js";
 import { SwitchDevice } from "./switch/index.js";
 import { VacuumDevice } from "./vacuum/index.js";
@@ -123,4 +129,28 @@ const matterDeviceTypeFactories: Partial<
   humidifier_dehumidifier: HumidifierDevice,
   speaker: MediaPlayerDevice,
   basic_video_player: VideoPlayerDevice,
+  humidity_sensor: (ha) =>
+    HumiditySensorType.set({
+      homeAssistantEntity: { entity: ha.entity, customName: ha.customName },
+    }),
+  temperature_sensor: (ha) =>
+    TemperatureSensorType.set({
+      homeAssistantEntity: { entity: ha.entity, customName: ha.customName },
+    }),
+  pressure_sensor: (ha) =>
+    PressureSensorType.set({
+      homeAssistantEntity: { entity: ha.entity, customName: ha.customName },
+    }),
+  light_sensor: (ha) =>
+    IlluminanceSensorType.set({
+      homeAssistantEntity: { entity: ha.entity, customName: ha.customName },
+    }),
+  flow_sensor: (ha) =>
+    FlowSensorType.set({
+      homeAssistantEntity: { entity: ha.entity, customName: ha.customName },
+    }),
+  air_quality_sensor: (ha) =>
+    AirQualitySensorType.set({
+      homeAssistantEntity: { entity: ha.entity, customName: ha.customName },
+    }),
 };
