00467b293c125c26410f66c7e9d9e0f059757d7f
RiDDiX <github@egu-rel.de>
2026-02-02 12:15:24 +0100

    fix: temporarily disable ServiceArea cluster to debug vacuum issue (#49)

--- packages/backend/src/matter/endpoints/legacy/vacuum/index.ts
+++ packages/backend/src/matter/endpoints/legacy/vacuum/index.ts
@@ -12,8 +12,10 @@ import { VacuumOnOffServer } from "./behaviors/vacuum-on-off-server.js";
 import { VacuumPowerSourceServer } from "./behaviors/vacuum-power-source-server.js";
 import { VacuumRvcOperationalStateServer } from "./behaviors/vacuum-rvc-operational-state-server.js";
 import { VacuumRvcRunModeServer } from "./behaviors/vacuum-rvc-run-mode-server.js";
-import { VacuumServiceAreaServer } from "./behaviors/vacuum-service-area-server.js";
-import { parseVacuumRooms } from "./utils/parse-vacuum-rooms.js";
+
+// TODO: ServiceArea cluster causes "Behaviors have errors" - needs debugging
+// import { VacuumServiceAreaServer } from "./behaviors/vacuum-service-area-server.js";
+// import { parseVacuumRooms } from "./utils/parse-vacuum-rooms.js";
 
 const VacuumEndpointType = RoboticVacuumCleanerDevice.with(
   BasicInformationServer,
@@ -47,10 +49,11 @@ export function VacuumDevice(
 
   // Add ServiceArea cluster if rooms/segments are available
   // This enables room selection in Apple Home (Matter 1.4 feature)
-  const rooms = parseVacuumRooms(attributes);
-  if (rooms.length > 0) {
-    device = device.with(VacuumServiceAreaServer);
-  }
+  // TODO: ServiceArea cluster causes "Behaviors have errors" - needs debugging
+  // const rooms = parseVacuumRooms(attributes);
+  // if (rooms.length > 0) {
+  //   device = device.with(VacuumServiceAreaServer);
+  // }
 
   return device;
 }
