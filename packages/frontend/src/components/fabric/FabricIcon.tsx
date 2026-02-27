3e6e13038c7a3679186e0414ea92708f3d99ce06
RiDDiX <github@egu-rel.de>
2026-02-27 12:49:20 +0100

    feat(frontend): add vendor brand icons for 18 new manufacturers

--- packages/frontend/src/components/fabric/FabricIcon.tsx
+++ packages/frontend/src/components/fabric/FabricIcon.tsx
@@ -4,9 +4,27 @@ import Box from "@mui/material/Box";
 import Tooltip from "@mui/material/Tooltip";
 import { type FC, type SVGProps, useMemo } from "react";
 import AmazonIcon from "../../assets/brands/Amazon.svg?react";
+import AnkerIcon from "../../assets/brands/Anker.svg?react";
 import AppleIcon from "../../assets/brands/Apple.svg?react";
+import AqaraIcon from "../../assets/brands/Aqara.svg?react";
+import AVMIcon from "../../assets/brands/AVM.svg?react";
+import DLinkIcon from "../../assets/brands/D-Link.svg?react";
+import DreameIcon from "../../assets/brands/Dreame.svg?react";
 import GoogleIcon from "../../assets/brands/Google.svg?react";
+import IKEAIcon from "../../assets/brands/IKEA.svg?react";
+import LGElectronicsIcon from "../../assets/brands/LG Electronics.svg?react";
+import LGThinQIcon from "../../assets/brands/LG ThinQ.svg?react";
+import LidlIcon from "../../assets/brands/Lidl.svg?react";
+import MerossIcon from "../../assets/brands/Meross.svg?react";
+import NukiIcon from "../../assets/brands/Nuki.svg?react";
+import PanasonicIcon from "../../assets/brands/Panasonic.svg?react";
 import SamsungIcon from "../../assets/brands/Samsung.svg?react";
+import SamsungSmartThingsIcon from "../../assets/brands/Samsung SmartThings.svg?react";
+import ShellyIcon from "../../assets/brands/Shelly.svg?react";
+import TPLinkIcon from "../../assets/brands/TP-Link.svg?react";
+import TuyaIcon from "../../assets/brands/Tuya.svg?react";
+import XiaomiIcon from "../../assets/brands/Xiaomi.svg?react";
+import YandexIcon from "../../assets/brands/Yandex.svg?react";
 import { getVendorName } from "./vendor-names.ts";
 
 export interface FabricIconProps {
@@ -15,18 +33,50 @@ export interface FabricIconProps {
 
 const iconsPerKeyword: Record<string, FC<SVGProps<SVGSVGElement>>> = {
   Alexa: AmazonIcon,
+  Amazon: AmazonIcon,
   Apple: AppleIcon,
   Google: GoogleIcon,
+  SmartThings: SamsungSmartThingsIcon,
+  Samsung: SamsungIcon,
+  IKEA: IKEAIcon,
+  Tuya: TuyaIcon,
+  Xiaomi: XiaomiIcon,
+  Aqara: AqaraIcon,
+  Shelly: ShellyIcon,
+  Meross: MerossIcon,
 };
 
 const iconPerVendorId: Record<number, FC<SVGProps<SVGSVGElement>> | undefined> =
   {
-    4631: AmazonIcon,
-    4937: AppleIcon,
-    4996: AppleIcon,
-    24582: GoogleIcon,
+    1: PanasonicIcon,
+    4142: LGElectronicsIcon,
     4321: SamsungIcon,
-    4362: SamsungIcon,
+    4362: SamsungSmartThingsIcon,
+    4417: TuyaIcon,
+    4442: LGThinQIcon,
+    4447: AqaraIcon,
+    4448: AmazonIcon,
+    4469: DLinkIcon,
+    4476: IKEAIcon,
+    4488: TPLinkIcon,
+    4631: AmazonIcon,
+    4701: TuyaIcon,
+    4718: XiaomiIcon,
+    4745: LidlIcon,
+    4757: AVMIcon,
+    4911: YandexIcon,
+    4933: MerossIcon,
+    4937: AppleIcon,
+    4957: NukiIcon,
+    4987: AmazonIcon,
+    4993: AmazonIcon,
+    4996: AppleIcon,
+    5009: TPLinkIcon,
+    5010: TPLinkIcon,
+    5264: ShellyIcon,
+    5420: DreameIcon,
+    5427: AnkerIcon,
+    24582: GoogleIcon,
   };
 
 function getIcon(fabric: BridgeFabric) {
